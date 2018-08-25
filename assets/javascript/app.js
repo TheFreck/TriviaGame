70b9774a6c6ec2a08f2dafe2686832f116924f46
Wilhelm Picaco <wpicaco@gmail.com>
2018-08-24 18:55:22 -0600

    initial upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,60 @@
+
+
+
+
+/*
+when player presses 'begin' a card pops up with a multiple choice or true or false question on it.
+
+they have some number of times they can miss a question
+
+track and graph how many cards the user gets each time they play to show potential improvement
+
+*/
+
+var trivia=[
+    {
+        question: "Question",
+        option1: "wrong answer" ,
+        option2: "wrong answer",
+        option3: "wrong answer",
+        answer: "answer"
+    },
+    {
+        question: "Question",
+        option1: "wrong answer" ,
+        option2: "wrong answer",
+        option3: "wrong answer",
+        answer: "answer"
+    },
+    {
+        question: "Question",
+        option1: "wrong answer" ,
+        option2: "wrong answer",
+        option3: "wrong answer",
+        answer: "answer"
+    },
+    {
+        question: "Question",
+        option1: "wrong answer" ,
+        option2: "wrong answer",
+        option3: "wrong answer",
+        answer: "answer"
+    }
+];
+    
+
+
+
+
+
+
+// create a begin function
+
+
+
+
+
+function begin(){
+    
+}
+
