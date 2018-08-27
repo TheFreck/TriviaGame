65af23802fc77b6be2a23d7d7a90aea87266f090
Wilhelm Picaco <wpicaco@gmail.com>
2018-08-26 23:06:43 -0600

    It has a a 30 second timer for you to get through as many questions as possible. After each click it moves to the next question. Still glitchy with the scoring

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,15 +2,8 @@
 
 
 
-/*
-when player presses 'begin' a card pops up with a multiple choice or true or false question on it.
 
-they have some number of times they can miss a question
-
-track and graph how many cards the user gets each time they play to show potential improvement
-
-*/
-
+// trivia questions
 var trivia=[
     {
         question: "Question1",
@@ -50,7 +43,7 @@ var trivia=[
         option2: "True",
         option3: "False",
         option4: "",
-        answer: "T/F"
+        answer: "True"
     },
     {
         question: "Question1",
@@ -90,7 +83,7 @@ var trivia=[
         option2: "True",
         option3: "False",
         option4: "",
-        answer: "T/F"
+        answer: "False"
     },
     {
         question: "Question1",
@@ -130,23 +123,29 @@ var trivia=[
         option2: "True",
         option3: "False",
         option4: "",
-        answer: "T/F"
+        answer: "True"
     },
 ];
 
-var inGame = false;
-$("#inGame").text("Game On: " + inGame);
 
-// create a begin function
+// **************************************************************************************************************
+// get started
+// ************************************************************************************************************** 
+
+function begin() {
+    gameTimer();
+    roll();
+}
+
 var qIndex;
 var aIndex;
+var rightAnswer;
+var answersArray;
 var score = 0;
-$("#score").text("Score: " + score);
+$("#score").text(score);
 
-function begin(){
-
-    inGame = true;
-    $("#inGame").text("Game On: " + inGame);
+function roll(){
+    
     // initialize
     $("#space").empty();
     var questionArray = [];
@@ -171,13 +170,11 @@ function begin(){
     
     qIndex = Math.floor(Math.random()*questionArray.length);
     
-    console.log("qIndex",qIndex);
     // randomize the answers
     
     aIndex = Math.floor(Math.random()*4);
-    console.log("aIndex",aIndex);
 
-    var answerArray = [
+    var optionsArray = [
         trivia[qIndex].option1,
         trivia[qIndex].option2,
         trivia[qIndex].option3,
@@ -185,20 +182,18 @@ function begin(){
         trivia[qIndex].option1,
         trivia[qIndex].option2,
         trivia[qIndex].option3,
-        trivia[qIndex].answer,
     ];
     
-    var answer1 = answerArray[aIndex];
-    var answer2 = answerArray[aIndex+1];
-    var answer3 = answerArray[aIndex+2];
-    var answer4 = answerArray[aIndex+3];
-    var rightAnswer = answerArray[7];
+    var answer1 = optionsArray[aIndex];
+    var answer2 = optionsArray[aIndex+1];
+    var answer3 = optionsArray[aIndex+2];
+    var answer4 = optionsArray[aIndex+3];
+    rightAnswer = trivia[qIndex].answer;
     
-    var answerArray = [answer1, answer2, answer3, answer4]
+    answersArray = [answer1, answer2, answer3, answer4]
+    var answersArrayString = ["answer1","answer2","answer3","answer4"];    
     
     //add answers to the card
-    
-    
 
     function addAnswers(){
         var answerList = $("<ul>");
@@ -207,8 +202,9 @@ function begin(){
         for(i=0; i<4; i++){
             var answers = $("<button>");
             answers.addClass("answer");
-            answers.attr("data", answerArray[i]);
-            answers.html("<p>" + answerArray[i] + "</p>");
+            answers.attr("id",answersArrayString[i]);
+            answers.attr("data", optionsArray[i]);
+            answers.html("<p>" + answersArray[i] + "</p>");
             answers.appendTo(answerList);
         }
         $(".card").append(answerList);
@@ -218,26 +214,72 @@ function begin(){
     addQuestion(qIndex);
     addAnswers();
 
-    // create a click event to select an answer
     
-    $(document).on("click", '.answer', function(){
-        if($(this).attr("data")===rightAnswer){
-            score ++;
-            $("#score").text("Score: " + score);
-            console.log("win or lose: win.",$(this).attr("data") + "=" + rightAnswer);
-        }else{
-            console.log("win or lose: lose.",rightAnswer);
-        }
-    });
-
     //finish the card
     if(trivia.length > 1){
         trivia.splice(qIndex,1);
     }else{
         $("#card").empty();
         $("#card").html("<h1>You Win!!!</h1>");
-        inGame = false;
-        $("#inGame").text("Game On: " + inGame);
     }
 }
 
+// **************************************************************************************************
+// a click event to select an answer
+// **************************************************************************************************
+
+$(document).on("click", '.answer', function(){
+    console.log("rightAnswer",rightAnswer);
+    console.log("this data",$(this).attr("data"));
+    if($(this).attr("data")===rightAnswer){
+        score ++;
+        console.log("score",score);
+        $("#score").text(score);
+        for(i=0; i<4; i++){
+            if(answersArray[i]===rightAnswer){
+                $(this).addClass("glow");
+            }
+        }
+    }
+    roll()
+});
+
+// ************************************************************************************************
+// timer
+// ************************************************************************************************
+
+
+function gameTimer(){
+    clearInterval(intervalId);
+    var seconds = 30;
+    var intervalId = setInterval(decrement, 1000);
+    function decrement(){
+        $("#timer").text(seconds);
+        seconds --;
+        $("#timer").text(seconds);
+    
+        if(seconds <= 0){
+            clearInterval(intervalId);
+            gameOver();
+        }
+    }
+}
+
+// **************************************************************************************************************
+// the end of times
+// **************************************************************************************************************
+
+function gameOver(){
+    $(".card").empty();
+    var question;
+    if(score != 1){
+        question = "questions";
+    }else{
+        question = "question";
+    }
+    $(".card").html("<h3>you got " + score + " " + question + " right!</h3>");
+}
+
+
+// hit or miss on whether it logs the correct answer
+// almost time to find questions
